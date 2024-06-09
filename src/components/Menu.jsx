155358e1ccf6dac69fc87d1a44fe3fb74edb6505
Pascal Brand <pascal.brand38@gmail.com>
2024-06-09 22:38:53 +0200

    more robustness
    Signed-off-by: Pascal Brand <pascal.brand38@gmail.com>

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -39,13 +39,25 @@ function Sign({tracks, setTracks, setFirstBounds, setSelectedTrack, setHoverTrac
         const track = convert.gpxToTrack(gpxXml, filename)
 
         // remove duplicates
-        tracks = tracks.filter(t => t.meta.epoch !== track.meta.epoch)
+        if (track.meta.epoch !== undefined) {
+          tracks = tracks.filter(t => t.meta.epoch !== track.meta.epoch)
+        }
+        tracks = tracks.filter(t => t.meta.gpxFilename !== track.meta.gpxFilename)
+
         tracks.push(track)
       })
     )
 
     // sort the tracks by start time
-    tracks.sort((a, b) => a.meta.epoch - b.meta.epoch)
+    tracks = tracks.sort((a, b) => {
+      if (a.meta.epoch === undefined) {
+        return +1
+      } else if (b.meta.epoch === undefined) {
+        return -1
+      } else {
+        return a.meta.epoch - b.meta.epoch
+      }
+    })
 
     // setTracks(tracks) does not rerender as tracks is not changed (still an array at the same address)
     setTracks([...tracks])
